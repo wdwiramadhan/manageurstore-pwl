a25bdfba56b099314561b4e703e621349f0c82e4
Wahyu Dwi Ramadhan <52495202+wdwiramadhan@users.noreply.github.com>
2020-04-14 23:57:52 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ if($_SERVER['HTTP_HOST'] != 'localhost'){
 }
 define('BASEURL', $TYPE . $_SERVER['HTTP_HOST'] . str_replace('public', '', dirname($_SERVER['SCRIPT_NAME'])));
 
-$DATABASE_URL=parse_url('postgres://bsbdslkinfdxdx:22405628b608bea73b5003a0f40a788881db8b9d61ea2595dc97f0e37b386a2e@ec2-3-211-48-92.compute-1.amazonaws.com:5432/dehvmki0abmqt3');
+$DATABASE_URL=parse_url('postgres://kwjegjqlvxsumw:6ed8f56052aa234c3681bb54935ae261be95f4c14618bb1bae523ecf23c059b0@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d6jldq50fef8ug
+');
 
-define('DB_DRIVER', 'pgsql');
 define('DB_HOST', $DATABASE_URL["host"]);
 define('DB_PORT', $DATABASE_URL["port"]);
 define('DB_NAME', ltrim( $DATABASE_URL["path"],"/");
